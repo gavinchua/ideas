2fc749d255eb7a49ff4a373af06130892d77a1d5
Gavin Chua <me@gavinchua.com>
2019-07-18 00:31:48 +0800

    add vuelidate & fortawesome

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlusSquare, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from '@/App';
 import router from '@/router';
 import store from '@/store';
 
 import '@/assets/css/main.css';
+
+Vue.use(Vuelidate);
+
+library.add(faPlusSquare, faTrashAlt);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
