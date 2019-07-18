4fa4239d1b8c5cad6a4785f39aae533689a575d2
Gavin Chua <me@gavinchua.com>
2019-07-19 00:06:03 +0800

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,29 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './public/**/*.html',
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx'
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  // defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: (content) => {
+    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+  },
+  whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ]
+});
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 };
