0fac48ae078ed56cdb5ae7b11101ea8ddc2cc303
Gavin Chua <me@gavinchua.com>
2019-07-18 23:45:48 +0800

    add vue-loaders

--- src/components/ideas-get.vue
+++ src/components/ideas-get.vue
@@ -3,48 +3,57 @@
     <h1 class="text-3xl font-bold mb-8">
       Ideas Board
     </h1>
-    <div class="flex justify-end -mx-4">
-      <a
-        class="inline-block mx-2"
-        href="#"
-        @click.prevent="sortByTitle"
-      >
-        Sort by Name
-      </a>
-      <a
-        class="inline-block mx-2"
-        href="#"
-        @click.prevent="sortByDate"
-      >
-        Sort by Date
-      </a>
-    </div>
-    <div class="flex flex-wrap -mx-4">
-      <div
-        v-for="i in getAppData"
-        :key="i.id"
-        class="my-4 px-4 w-full overflow-hidden sm:w-1/2 md:w-1/3 lg:w-1/4"
-      >
-        <div class="bg-gray-400 hover:bg-gray-500 hover:text-white px-4 py-6 relative item">
-          <h5 class="mb-1">
-            {{ i.title }}
-          </h5>
-          <p class="text-xs mb-3">
-            {{ i.created_date }}
-          </p>
-          <p class="text-sm">
-            {{ i.body }}
-          </p>
-          <a
-            href="#"
-            class="absolute hidden"
-            @click.prevent="deleteItem(i.id)"
-          >
-            <font-awesome-icon icon="trash-alt" />
-          </a>
+    <template v-if="isLoading">
+      <vue-loaders-ball-beat
+        class="w-full text-center"
+        color="red"
+        scale="1"
+      />
+    </template>
+    <template v-else>
+      <div class="flex justify-end -mx-4">
+        <a
+          class="inline-block mx-2"
+          href="#"
+          @click.prevent="sortByTitle"
+        >
+          Sort by Name
+        </a>
+        <a
+          class="inline-block mx-2"
+          href="#"
+          @click.prevent="sortByDate"
+        >
+          Sort by Date
+        </a>
+      </div>
+      <div class="flex flex-wrap -mx-4">
+        <div
+          v-for="i in getAppData"
+          :key="i.id"
+          class="my-4 px-4 w-full overflow-hidden sm:w-1/2 md:w-1/3 lg:w-1/4"
+        >
+          <div class="bg-gray-400 hover:bg-gray-500 hover:text-white px-4 py-6 relative item">
+            <h5 class="mb-1">
+              {{ i.title }}
+            </h5>
+            <p class="text-xs mb-3">
+              {{ i.created_date }}
+            </p>
+            <p class="text-sm">
+              {{ i.body }}
+            </p>
+            <a
+              href="#"
+              class="absolute hidden"
+              @click.prevent="deleteItem(i.id)"
+            >
+              <font-awesome-icon icon="trash-alt" />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -54,10 +63,20 @@ import { mapGetters } from 'vuex';
 
 export default {
   name: 'IdeasGet',
+  data() {
+    return {
+      isLoading: true
+    };
+  },
   computed: {
     ...mapGetters('appData', [
       'getAppData'
     ])
+  },
+  mounted() {
+    if (this.getAppData) {
+      this.isLoading = false;
+    }
   },
   methods: {
     sortByTitle() {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueLoadersBallBeat from 'vue-loaders/dist/loaders/ball-beat';
 import Vuelidate from 'vuelidate';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlusSquare, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
@@ -8,8 +9,10 @@ import App from '@/App';
 import router from '@/router';
 import store from '@/store';
 
+import 'vue-loaders/dist/vue-loaders.css';
 import '@/assets/css/main.css';
 
+Vue.use(VueLoadersBallBeat);
 Vue.use(Vuelidate);
 
 library.add(faPlusSquare, faTrashAlt);
