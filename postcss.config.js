49a9ca5c87925062e810bcc0d5856d237ec5dd2a
Gavin Chua <me@gavinchua.com>
2019-07-20 00:22:24 +0800

    add whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -15,6 +15,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
     return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
   },
+  whitelist: [ 'ol', 'ul' ],
   whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ]
 });
 
