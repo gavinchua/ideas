69eff6ebc9a2b03782b6fab8af79c1236e83a19f
Gavin Chua <me@gavinchua.com>
2019-07-20 00:50:50 +0800

    add github repo

--- src/views/shared/Footer.vue
+++ src/views/shared/Footer.vue
@@ -1,10 +1,18 @@
 <template>
-  <footer>
-    &copy;
-    <span
-      v-text="getThisYear"
-    />
-    @gavin_chua
+  <footer class="md:flex">
+    <div class="text-center md:w-1/2 md:text-left">
+      &copy;
+      <span
+        v-text="getThisYear"
+      />
+      @gavin_chua
+    </div>
+    <div class="text-center md:w-1/2 md:text-right">
+      <a
+        href="https://github.com/gavinchua/ideas"
+        target="_blank"
+      >Github</a>
+    </div>
   </footer>
 </template>
 
