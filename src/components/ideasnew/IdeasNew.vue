57625fa1d60794ff2ccaac7f562e2423656080c0
Gavin Chua <me@gavinchua.com>
2019-07-20 13:01:41 +0800

    components restructuring

---
+++ src/components/ideasnew/IdeasNew.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ideasnew">
+    <h1 class="text-3xl font-bold mb-8">
+      Create a new idea
+    </h1>
+    <IdeasNewItem />
+  </div>
+</template>
+
+<script>
+import IdeasNewItem from '@/components/ideasnew/IdeasNewItem';
+
+export default {
+  name: 'IdeasNew',
+  components: {
+    IdeasNewItem
+  },
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ src/components/ideasnew/IdeasNewItem.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="ideasnewitem">
+    <form
+      id="formCreate"
+      action=""
+      @submit.prevent="submit"
+    >
+      <div class="mb-4">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="title"
+        >
+          Title
+        </label>
+        <input
+          id="title"
+          v-model="idea.title"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="title"
+        >
+        <div
+          v-if="submitted && $v.idea.title.$error"
+          class="text-red-500 text-xs italic"
+        >
+          <template v-if="submitted && !$v.idea.title.required">
+            Required.
+          </template>
+          <template v-if="submitted && !$v.idea.title.minLength">
+            Length must be at least 2 characters.
+          </template>
+        </div>
+      </div>
+      <div class="mb-6">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="body"
+        >
+          Body
+        </label>
+        <input
+          id="body"
+          v-model.trim="$v.idea.body.$model"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="body"
+        >
+        <div
+          v-if="submitted && $v.idea.body.$error"
+          class="text-red-500 text-xs italic"
+        >
+          <template v-if="submitted && !$v.idea.body.required">
+            Required.
+          </template>
+          <template v-if="submitted && !$v.idea.body.minLength">
+            Length must be at least 5 characters.
+          </template>
+        </div>
+      </div>
+      <div class="mb-6">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Submit
+        </button>
+        <router-link
+          class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded ml-3"
+          tag="button"
+          to="/"
+        >
+          Cancel
+        </router-link>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { required, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'IdeasNewItem',
+  filters: {
+    json: function(value) {
+      return JSON.stringify(value, null, '\t');
+    }
+  },
+  data() {
+    return {
+      idea: {
+        title: '',
+        body: '',
+        created_date: new Date()
+      },
+      submitted: false
+    };
+  },
+  validations: {
+    idea: {
+      title: {
+        required,
+        minLength: minLength(2)
+      },
+      body: {
+        required,
+        minLength: minLength(5)
+      }
+    }
+  },
+  methods: {
+    submit() {
+      this.submitted = true;
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      } else {
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.idea));
+        this.sendFormData();
+      }
+    },
+    sendFormData() {
+      console.log(this.idea);
+      // dispatch should be performed after axios.post
+      // question is how to get the id for the new data?
+      // auto from api/backend?
+      this.$store.dispatch('appData/commitAddAppData', this.idea);
+      this.$router.push('home');
+      axios.post(`http://www.amock.io/api/gavinchua/idea/new/${this.idea}`)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
